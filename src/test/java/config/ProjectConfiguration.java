5b74fa645d29c0145adb70d474e983dbec80181f
Nikolay <dojazz@icloud.com>
2023-07-07 11:17:00 +0300

    refactor build added task "any"

---
+++ src/test/java/config/ProjectConfiguration.java
@@ -0,0 +1,40 @@
+package config;
+
+
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class ProjectConfiguration {
+    private final WebConfig webConfig;
+
+    public ProjectConfiguration(WebConfig webConfig) {
+        this.webConfig = webConfig;
+    }
+
+    public void apiConfig() {
+        RestAssured.baseURI = webConfig.baseUrl();
+    }
+
+    public void webConfig() {
+        Configuration.baseUrl = webConfig.baseUrl();
+        Configuration.browser = webConfig.browser().toString();
+        Configuration.browserVersion = webConfig.browserVersion();
+        Configuration.browserSize = webConfig.browserSize();
+        if (webConfig.isRemote()) {
+            Configuration.remote = webConfig.remoteUrl();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+        }
+    }
+
+    public String getVideoStorageUrl() {
+        return webConfig.videoStorageUrl();
+    }
+
+    public Boolean isRemote() {
+        return webConfig.isRemote();
+    }
+}
---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,40 @@
+package config;
+
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+//        "system:properties",
+        "classpath:remote.properties",
+//        "file:~/${env}.properties",
+//        "file:./${env}.properties"
+})
+public interface WebConfig extends Config {
+    @Key("browser")
+    @DefaultValue("CHROME")
+    Browser browser();
+
+    @Key("browserVersion")
+    @DefaultValue("106.0")
+    String browserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+    @Key("baseUrl")
+    @DefaultValue("https://www.redcollar.ru")
+    String baseUrl();
+
+    @Key("isRemote")
+    @DefaultValue("false")
+    boolean isRemote();
+
+    @Key("remoteUrl")
+    @DefaultValue("http://localhost:4444")
+    String remoteUrl();
+
+    @Key("videoStorageUrl")
+    String videoStorageUrl();
+}
